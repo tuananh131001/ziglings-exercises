5075dfd187ae9ccc3a57f63244d1679b1bb6a0e6
tuananh131001 <tuananh131001@gmail.com>
2025-09-28 16:55:42 +0700

    finished 34

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,10 +9,10 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void { // add all raise exception here
     var stdout = std.fs.File.stdout().writer(&.{});
 
-    const my_num: u32 = getNumber();
+    const my_num: u32 = try getNumber(); // try for shorter syntax than catch return
 
     try stdout.interface.print("my_num={}\n", .{my_num});
 }
